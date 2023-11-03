709767e7afbb2fd963979626bc9d5dee93fdc9de
riccardo-giuri <richicryst@gmail.com>
2023-11-03 23:05:24 +0100

    filter API functionality DONE

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -5,12 +5,40 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Psy\Readline\Hoa\Console;
+use Illuminate\Support\Facades\Log;
 
 class RestaurantController extends Controller
 {
-    public function index() {
-        $restaurants = Restaurant::with("cuisines")->get(); 
+    public function index(Request $request) {
+        $cuisines = $request->all();
+        
+        if(count($cuisines) <= 0) {
+            $restaurants = Restaurant::with("cuisines")->get();
 
-        return response()->json($restaurants);
+            return response()->json($restaurants);
+        }
+        else {  
+            $query = Restaurant::select('restaurants.*');
+
+            $aliasCounter = 1;
+
+            foreach ($cuisines as $cuisine) {
+                $cAlias = 'c' . $aliasCounter;
+                $crAlias = 'cr'. $aliasCounter;
+ 
+                $query->join("cuisine_restaurant as $crAlias", 'restaurants.id', '=', "$crAlias.restaurant_id")
+                    ->join("cuisines as $cAlias", function ($join) use ($cuisine, $cAlias, $crAlias) {
+                        $join->on("$crAlias.cuisine_id", '=', "$cAlias.id")
+                            ->where("$cAlias.cuisine_name", '=', $cuisine);
+                });
+
+                $aliasCounter++;
+            }
+    
+            $restaurants = $query->distinct()->with('cuisines')->get();     
+            
+            return response()->json($restaurants);
+        }   
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CuisineController;
 use App\Http\Controllers\Api\RestaurantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//API for restaurants
 Route::get("restaurants", [RestaurantController::class,"index"]);
+
+//API for cuisines
+Route::get("cuisines", [CuisineController::class,"index"]);
