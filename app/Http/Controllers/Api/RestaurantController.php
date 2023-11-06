bbe110130abe3255493400be8b6f6fb63fbbc8a9
riccardo-giuri <richicryst@gmail.com>
2023-11-06 12:09:54 +0100

    comments in the code added

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -11,33 +11,45 @@ use Illuminate\Support\Facades\Log;
 class RestaurantController extends Controller
 {
     public function index(Request $request) {
+        //take cuisines data from HTTP request
         $cuisines = $request->all();
         
+        //check if there is any cuisines coming from HTTP request 
         if(count($cuisines) <= 0) {
+            //get all restaurants from DB
             $restaurants = Restaurant::with("cuisines")->get();
 
+            //return JSON of them to frontend
             return response()->json($restaurants);
         }
         else {  
+            //select of right model
             $query = Restaurant::select('restaurants.*');
 
+            //alias counter used in the foreach query
             $aliasCounter = 1;
 
+            //loop on every cuisine selected by Client frontend
             foreach ($cuisines as $cuisine) {
+                //aliases changes for every iteration
                 $cAlias = 'c' . $aliasCounter;
                 $crAlias = 'cr'. $aliasCounter;
  
+                //query for retriving the restaurants with the different cuisine selected
                 $query->join("cuisine_restaurant as $crAlias", 'restaurants.id', '=', "$crAlias.restaurant_id")
                     ->join("cuisines as $cAlias", function ($join) use ($cuisine, $cAlias, $crAlias) {
                         $join->on("$crAlias.cuisine_id", '=', "$cAlias.id")
                             ->where("$cAlias.cuisine_name", '=', $cuisine);
                 });
 
+                //counter add every iteration
                 $aliasCounter++;
             }
     
+            //clean the results of the query of duplicates and save them
             $restaurants = $query->distinct()->with('cuisines')->get();     
             
+            //return JSON of them to frontend
             return response()->json($restaurants);
         }   
     }
