bbefca36e8e686e8035a3db54ff96cf49d39c36c
DarioEsposito89 <darioesposito.work@gmail.com>
2023-11-08 16:06:30 +0100

    fixed model Order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,6 +10,15 @@ class Order extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        "customer_name",
+        "customer_lastname",
+        "customer_email",
+        "customer_address",
+        "customer_phone",
+        "ammount_paid",
+    ];
+
     // Many to many relation to plate
     public function plates() {
         return $this->belongsToMany(Plate::class);
