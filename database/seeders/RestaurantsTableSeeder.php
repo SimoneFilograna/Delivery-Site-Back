14e584fc4b15ca73c3774a36b942362aacd41294
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-11-09 16:28:28 +0100

    fix seeder image

--- database/seeders/RestaurantsTableSeeder.php
+++ database/seeders/RestaurantsTableSeeder.php
@@ -15,7 +15,7 @@ class RestaurantsTableSeeder extends Seeder
         [
             "restaurant_name" => "Tokyo Fire",
             "restaurant_address" => "Via Brombeis, 2",
-            "restaurant_image" => "https://www.torinotoday.it/~media/horizontal-hi/46538377512956/umami_ristorante_giapponese_centro_torino-2.jpg",
+            "restaurant_image" => "restaurants/tokyo-fire.jpg",
             "restaurant_phone" => "02 1234567",
             "vat_number" => "12345678901",
             "user_id" => 1
@@ -23,7 +23,7 @@ class RestaurantsTableSeeder extends Seeder
         [
             "restaurant_name" => "Mexico Loco",
             "restaurant_address" => "Via lauridis, 23",
-            "restaurant_image" => "https://www.viaggiaregratis.eu/wp-content/uploads/2020/11/Cucina-Messicana.jpg",
+            "restaurant_image" => "restaurants/mexico-loco.jpg",
             "restaurant_phone" => "02 4592347",
             "vat_number" => "73497120482",
             "user_id" => 2
@@ -31,7 +31,7 @@ class RestaurantsTableSeeder extends Seeder
         [
             "restaurant_name" => "Pizza Party",
             "restaurant_address" => "Via Pompadur, 43",
-            "restaurant_image" => "https://nepomuk.it/wp-content/uploads/2022/02/pizzeria-sterzing.jpg",
+            "restaurant_image" => "restaurants/pizza-partyg.jpg",
             "restaurant_phone" => "02 7284565",
             "vat_number" => "71492120588",
             "user_id" => 3
@@ -39,7 +39,7 @@ class RestaurantsTableSeeder extends Seeder
         [
             "restaurant_name" => "Indian Family",
             "restaurant_address" => "Via ponticello, 78",
-            "restaurant_image" => "https://media-cdn.tripadvisor.com/media/photo-s/1b/d3/9e/d8/getlstd-property-photo.jpg",
+            "restaurant_image" => "restaurants/indian-family.jpg",
             "restaurant_phone" => "02 2392940",
             "vat_number" => "23457129499",
             "user_id" => 4
@@ -55,7 +55,7 @@ class RestaurantsTableSeeder extends Seeder
             $newRestaurant = new Restaurant();
             $newRestaurant->restaurant_name = $restaurant["restaurant_name"];
             $newRestaurant->restaurant_address = $restaurant["restaurant_address"];
-            $newRestaurant->restaurant_image = $restaurant["restaurant_image"];
+            $newRestaurant->restaurant_image = "/{$restaurant['restaurant_image']}";
             $newRestaurant->restaurant_phone = $restaurant["restaurant_phone"];
             $newRestaurant->vat_number = $restaurant["vat_number"];          
             $newRestaurant->user_id = $restaurant["user_id"];
